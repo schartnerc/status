c36e8f989982da0a60a8a9e7a9a0f711337f46e0
Christoph <schartnerc@etown.edu>
2019-04-08 00:03:22 -0400

    Added functionality

--- app.py
+++ app.py
@@ -1,12 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template, current_app
+import yaml
+import os
+
 
 app = Flask(__name__)
 
+def update_process(processes):
+	context = []
+	for process in processes.keys():
+		command = 'pgrep -fx "' + processes[process] + '"'
+		context.append({'name': process, 'status': os.system(command) != 256})
+
+	return context
+
+
+
+def read_yaml():
+	with open("process.yaml", 'r') as stream:
+		try:
+			return yaml.safe_load(stream)
+		except yaml.YAMLError:
+			print('YAML error')
 
 @app.route('/')
 def hello_world():
-	return 'Hello World!'
-
+	return render_template('status.html', services=update_process(read_yaml()))
 
 if __name__ == '__main__':
+	global process
+	process = read_yaml()
+	print(process)
 	app.run()
